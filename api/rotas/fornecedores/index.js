bc699c5e90124b175650f5b97fbcf76c7b5e7581
LeandroMenoci <menoci.leandro@gmail.com>
2022-04-04 12:37:53 -0300

    Stores de uma API RESTful / RElacionamento de tabelas com Sequelize /  Definir rotas e hierarquia de dados em uima API RESTful

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -77,5 +77,8 @@ roteador.delete('/:idFornecedor', async (requisicao, resposta, proximo) => {
   }
 })
 
+const roteadorProdutos = require('./produtos')
+roteador.use('/:idFornecedor/produtos', roteadorProdutos)
+
 
 module.exports = roteador
